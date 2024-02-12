8c14d69f7e3ef1830af86634420ad0e3b651dc5b
DESKTOP-TMBJSNJ\enrique <cheta151@hotmail.com>
2024-02-12 20:09:52 +0100

    add input and output to custom js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,20 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
-  core.notice("Hello from my custom Javascript action");
+  // 1) get some input values
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // 2) upload files
+  exec.run('echo "uploading files..."');
+  const s3Uri = `s3://${bucket}`;
+  // exec.run(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  core.notice(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
 }
 
 run();
